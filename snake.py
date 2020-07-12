88f302708ea9b05192939e2b6c187c5df48d7888
qianqian <422374982qq@gmail.com>
2020-07-11 23:19:15 -0400

    game1

---
+++ snake.py
@@ -0,0 +1,83 @@
+﻿import time
+import random
+import pygame
+from pygame.locals import *
+
+
+
+pygame.init()
+SCREEN_RECT = pygame.Rect(0, 0, 1000, 750)
+OFFSET = 10
+fps_clock = pygame.time.Clock()
+screen = pygame.display.set_mode(SCREEN_RECT.size)
+pygame.display.set_caption("贪吃蛇")
+gameover_font = pygame.font.SysFont("arial", 100)
+gameover_text = gameover_font.render("Game Over", True, (150, 150, 150))
+gameover_rect = gameover_text.get_rect()
+gameover_rect.center = SCREEN_RECT.center
+snake_position = [100, 100]
+snake_segments = [[100, 100], [100 - OFFSET, 100], [100 - 2 * OFFSET, 100]]
+target_position = [300, 300]
+flag = 0
+direction = 'right'
+temp_direction = direction
+while True:
+    fps_clock.tick(20)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[K_RIGHT]:
+        temp_direction = 'right'
+    if keys_pressed[K_LEFT]:
+        temp_direction = 'left'
+    if keys_pressed[K_UP]:
+        temp_direction = 'up'
+    if keys_pressed[K_DOWN]:
+        temp_direction = 'down'
+    if (((temp_direction == 'right') and (direction != 'left'))
+            or ((temp_direction == 'left') and (direction != 'right'))
+            or ((temp_direction == 'up') and (direction != 'down'))
+            or (temp_direction == 'down') and (direction != 'up')):
+        direction = temp_direction
+    if direction == 'right':
+        snake_position[0] += OFFSET
+    if direction == 'left':
+        snake_position[0] -= OFFSET
+    if direction == 'up':
+        snake_position[1] -= OFFSET
+    if direction == 'down':
+        snake_position[1] += OFFSET
+    snake_segments.insert(0, list(snake_position))
+    print(snake_segments)
+    if (snake_position[0] == target_position[0]) and (snake_position[1] == target_position[1]):
+        flag = 1
+    else:
+        snake_segments.pop()
+    if flag:
+        x = random.randrange(1, SCREEN_RECT.width // OFFSET)
+        y = random.randint(1, SCREEN_RECT.height // OFFSET)
+        target_position = [int(x * OFFSET), int(y * OFFSET)]
+        flag = 0
+    screen.fill((0, 0, 0))
+    for each in snake_segments:
+        pygame.draw.rect(screen, (255, 255, 255), Rect(each[0], each[1], OFFSET, OFFSET))
+    pygame.draw.rect(screen, (255, 0, 0), Rect(target_position[0], target_position[1], OFFSET, OFFSET))
+    pygame.display.update()
+    if ((snake_position[0] < 0)
+            or (snake_position[0] > SCREEN_RECT.width - OFFSET)
+            or (snake_position[1] < 0)
+            or (snake_position[1] > SCREEN_RECT.height - OFFSET)):
+        screen.blit(gameover_text, gameover_rect)
+        pygame.display.update()
+        time.sleep(2)
+        pygame.quit()
+        exit()
+    for each in snake_segments[1:]:
+        if (snake_segments[0] == each[0]) and (snake_segments[1] == each[1]):
+            screen.blit(gameover_text, gameover_rect)
+            pygame.display.update()
+            time.sleep(2)
+            pygame.quit()
+            exit()
---
+++ snaker_eat.py
@@ -0,0 +1,95 @@
+﻿import pygame
+import sys
+import random
+
+
+def main():
+    pygame.init()
+    pygame.mixer.music.load("bg.wav")
+    pygame.mixer.music.play(-1, 0)
+    Width, Height = 420, 420
+    window = pygame.display.set_mode((Width, Height))
+    pygame.display.set_caption('snake game')
+    direct = 'right'
+    head_color = (255, 0, 0)
+    head = [30, 15]
+    snake_body = [[30, 15], [15, 15], [0, 15]]
+    clock = pygame.time.Clock()
+
+    food_POSITION = [random.randint(1, 27) * 15, random.randint(1, 27) * 15]
+    food_pos = False
+    _font = pygame.font.SysFont("arial", 60)
+    _text = _font.render("Game Over", True, (255, 255, 255))
+    font = pygame.font.SysFont("arial", 30)
+    text = font.render("SPACE TO RESTART", 1, (255, 255, 255))
+    while quit:
+        clock.tick(10)
+        snake_body.insert(0, list(head))
+        if head[0] == food_POSITION[0] and head[1] == food_POSITION[1]:
+            food_pos = True
+            print(snake_body)
+
+        else:
+            snake_body.pop()
+        if food_pos:
+
+            food_POSITION = [random.randint(1, 27) * 15, random.randint(1, 27) * 15]
+            if food_POSITION[0] == head[0] and food_POSITION[1] == head[1]:
+                pass
+            if food_POSITION[0] >= 0 or food_POSITION[0] <= [Width]:
+                pass
+            if food_POSITION[1] >= 0 or food_POSITION[1] <= [Height]:
+                pass
+            for snake in snake_body:
+                if food_POSITION[0] == snake[0] and food_POSITION[1] == snake[1]:
+                    pass
+            else:
+                food_pos = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    main()
+                if event.key == 273 or event.key == 119:
+                    if direct == 'right' or direct == 'left':
+                        direct = 'top'
+                if event.key == 274 or event.key == 115:
+                    if direct == 'left' or direct == 'right':
+                        direct = 'bottom'
+                if event.key == 276 or event.key == 97:
+                    if direct == 'top' or direct == 'bottom':
+                        direct = 'left'
+                if event.key == 275 or event.key == 100:
+                    if direct == 'bottom' or direct == 'top':
+                        direct = 'right'
+        if direct == "right":
+            head[0] += 15
+        if direct == 'left':
+            head[0] -= 15
+        if direct == 'top':
+            head[1] -= 15
+        if direct == 'bottom':
+            head[1] += 15
+
+        window.fill((0, 0, 0))
+        for snake in snake_body:
+            pygame.draw.rect(window, head_color, (snake[0], snake[1], 15, 15))
+
+        pygame.draw.rect(window, head_color, (food_POSITION[0], food_POSITION[1], 15, 15))
+
+        for snake in snake_body[1:]:
+            if head[0] == snake[0] and head[1] == snake[1]:
+                window.fill((0, 0, 0))
+                window.blit(_text, (80, 180))
+                window.blit(text, (90, 225))
+        if head[0] < 0 or head[0] > Width or head[1] > Height or head[1] < 0:
+            window.fill((0, 0, 0))
+            window.blit(_text, (80, 160))
+            window.blit(text, (90, 225))
+
+        pygame.display.update()
+
+
+main()
